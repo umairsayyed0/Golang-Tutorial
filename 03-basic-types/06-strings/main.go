82d1875ed6358902656f919eb8f086f857f42189
umairsayyed0 <68083351+umairsayyed0@users.noreply.github.com>
2022-01-31 16:15:43 +0530

    Update main.go

--- 03-basic-types/06-strings/main.go
+++ 03-basic-types/06-strings/main.go
@@ -4,12 +4,10 @@ import "fmt"
 
 func main() {
 	// Normal String (Can not contain newlines, and can have escape characters like `\n`, `\t` etc)
-	var website = "\thttps://www.callicoder.com\t\n"
+	var website = "\thttps://www.Google.com\t\n"
 
 	// Raw String (Can span multiple lines. Escape characters are not interpreted)
-	var siteDescription = `\t\tCalliCoder is a programming blog where you can find
-                           practical guides and tutorials on programming languages, 
-                           web development, and desktop app development.\t\n`
+	var siteDescription = `\t\tGoole is a search engine which is widely used by millions of users around the World.\t\n`
 
 	fmt.Println(website, siteDescription)
 }
